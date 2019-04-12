26ea9be3fd04853cc2bb6259fecc3ad38fa062f1
John Kennedy Akabo <akabojohnkennedy@gmail.com>
2019-04-12 16:52:38 +0000

    Removed .idea folder

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Pharmacy(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Staff(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone = models.CharField(max_length=9)
+
+    pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
+
--- pharma/settings.py
+++ pharma/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'accounts.Staff'
