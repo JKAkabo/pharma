811d658a3249b8c30987b63caa9b7ac4660f7850
John Kennedy Akabo <akabojohnkennedy@gmail.com>
2019-04-22 07:10:06 +0000

    Updated Pipfile and created AddProductForm

--- shelf/forms.py
+++ shelf/forms.py
@@ -6,3 +6,11 @@ from .models import Product, Stock, GroupSale
 class AddProductForm(forms.ModelForm):
     class Meta:
         model = Product
+        fields = (
+            'name',
+            'active_ingredients',
+        )
+
+
+class AddStockForm(forms.Form):
+    units = forms.IntegerField(min_value=1)
