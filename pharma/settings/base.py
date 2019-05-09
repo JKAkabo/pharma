3b27e0df44f04d6933e2b1d413ed91a6ae358679
John Kennedy Akabo <=>
2019-05-09 06:25:52 +0000

    Preparations for production

--- pharma/settings/base.py
+++ pharma/settings/base.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'accounts.apps.AccountsConfig',
     'shelf.apps.ShelfConfig',
 ]
--- pharma/settings/production.py
+++ pharma/settings/production.py
@@ -1,5 +1,11 @@
+import django_heroku
+
 from .base import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
